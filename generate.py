505814580ae68f6a8e5e01b5026a25d8bf77c7d4
Emma-Havens <emmahavens2023@u.northwestern.edu>
2023-01-09 16:52:54 -0600

    Manylinks

--- generate.py
+++ generate.py
@@ -1,5 +1,30 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+pyrosim.Start_SDF("boxes.sdf")
+
+length = 1
+width = 1
+height = 1
+
+x = 0
+y = 0
+z = .5
+
+for r in range(5):
+    for c in range(5):
+        for i in range(10):
+            boxName = 'Box' + str(i)
+            pyrosim.Send_Cube(name=boxName, pos=[x, y, z] , size=[width, length, height])
+            length = length * .9
+            width = width * .9
+            height = height * .9
+            z += 1
+        length = 1
+        width = 1
+        height = 1
+        z = .5
+        y += 1
+    y = 0
+    x += 1    
+
 pyrosim.End()
