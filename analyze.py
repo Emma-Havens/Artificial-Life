bc6859a9794f052ae8b5a2e7bfc2f250adde5088
Emma-Havens <emmahavens2023@u.northwestern.edu>
2023-01-16 22:42:03 -0600

    motor done

--- analyze.py
+++ analyze.py
@@ -3,11 +3,17 @@ import matplotlib.pyplot as matplot
 
 backLegSensorValues = np.load('data/grounded.npy')
 frontLegSensorValues = np.load('data/front_grounded.npy')
+targetAngleFront = np.load('data/sinusoidal_front.npy')
+targetAngleBack = np.load('data/sinusoidal_back.npy')
 print(backLegSensorValues)
 
 matplot.plot(backLegSensorValues, label='back leg', linewidth=4)
 matplot.plot(frontLegSensorValues, label='front leg')
 
 matplot.legend()
+matplot.show()
+
+matplot.plot(targetAngleFront)
+matplot.plot(targetAngleBack)
 matplot.show()
 
